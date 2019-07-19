ddd0b5aacb03a3754f9fc71e019f932e690e3b77
hugounavez <hugo.reyesamell@gmail.com>
2019-07-19 01:16:09 -0400

    base file projects added

---
+++ geniusplaza/geniusplaza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  geniusplaza
+//
+//  Created by Hugo Reyes on 7/19/19.
+//  Copyright © 2019 Hugo Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
